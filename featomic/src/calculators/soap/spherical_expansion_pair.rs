Luthaf/rascaline

Small changes in code organisation for next 2 commits
[restructured contribution, commit 1]

rascaline/src/calculators/soap/spherical_expansion_pair.rs
@@ -17,6 +17,7 @@ use super::super::neighbor_list::FullNeighborList;
 use super::{CutoffFunction, RadialScaling};
 
 use crate::calculators::radial_basis::RadialBasis;
+use crate::calculators::GradientsOptions;
 use super::SoapRadialIntegralCache;
 
 use super::radial_integral::SoapRadialIntegralParameters;
@@ -78,32 +79,17 @@ pub struct SphericalExpansionByPair {
     pub(crate) parameters: SphericalExpansionParameters,
     /// implementation + cached allocation to compute the radial integral for a
     /// single pair
-    radial_integral: ThreadLocal<RefCell<SoapRadialIntegralCache>>,
+    pub(crate) radial_integral: ThreadLocal<RefCell<SoapRadialIntegralCache>>,
     /// implementation + cached allocation to compute the spherical harmonics
     /// for a single pair
-    spherical_harmonics: ThreadLocal<RefCell<SphericalHarmonicsCache>>,
+    pub(crate) spherical_harmonics: ThreadLocal<RefCell<SphericalHarmonicsCache>>,
     /// Cache for (-1)^l values
-    m_1_pow_l: Vec<f64>,
+    pub(crate) m_1_pow_l: Vec<f64>,
 }
 
 impl std::fmt::Debug for SphericalExpansionByPair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self.parameters)
-    }
-}
-
-
-/// Which gradients are we computing
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(super) struct GradientsOptions {
-    pub positions: bool,
-    pub cell: bool,
-    pub strain: bool,
-}
-
-impl GradientsOptions {
-    pub fn any(self) -> bool {
-        return self.positions || self.cell || self.strain;
     }
 }
 
@@ -193,14 +179,14 @@ impl SphericalExpansionByPair {
     }
 
     /// Compute the product of radial scaling & cutoff smoothing functions
-    fn scaling_functions(&self, r: f64) -> f64 {
+    pub(crate) fn scaling_functions(&self, r: f64) -> f64 {
         let cutoff = self.parameters.cutoff_function.compute(r, self.parameters.cutoff);
         let scaling = self.parameters.radial_scaling.compute(r);
         return cutoff * scaling;
     }
 
     /// Compute the gradient of the product of radial scaling & cutoff smoothing functions
-    fn scaling_functions_gradient(&self, r: f64) -> f64 {
+    pub(crate) fn scaling_functions_gradient(&self, r: f64) -> f64 {
         let cutoff = self.parameters.cutoff_function.compute(r, self.parameters.cutoff);
         let cutoff_grad = self.parameters.cutoff_function.derivative(r, self.parameters.cutoff);
 

python/rascaline/rascaline/systems/pyscf.py
@@ -92,7 +92,7 @@ class PyscfSystem(SystemBase):
         return self._types
 
     def positions(self):
-        return pyscf.data.nist.BOHR * self._frame.atom_coords()
+        return self._frame.atom_coords("angstrom")
 
     def cell(self):
         if self.is_periodic:

metatensor/featomic

featomic/src/calculators/soap/spherical_expansion_pair.rs
@@ -13,7 +13,7 @@ use super::super::shared::{Density, SoapRadialBasis, SphericalExpansionBasis};
 
 use crate::math::SphericalHarmonicsCache;
 
-use super::super::CalculatorBase;
+use crate::calculators::{CalculatorBase,GradientsOptions};
 use super::super::neighbor_list::FullNeighborList;
 
 use super::SoapRadialIntegralCacheByAngular;
@@ -64,32 +64,17 @@ pub struct SphericalExpansionByPair {
     pub(crate) parameters: SphericalExpansionParameters,
     /// implementation + cached allocation to compute the radial integral for a
     /// single pair
-    radial_integral: ThreadLocal<RefCell<SoapRadialIntegralCacheByAngular>>,
+    pub(crate) radial_integral: ThreadLocal<RefCell<SoapRadialIntegralCacheByAngular>>,
     /// implementation + cached allocation to compute the spherical harmonics
     /// for a single pair
-    spherical_harmonics: ThreadLocal<RefCell<SphericalHarmonicsCache>>,
+    pub(crate) spherical_harmonics: ThreadLocal<RefCell<SphericalHarmonicsCache>>,
     /// Cache for (-1)^l values
-    m_1_pow_l: Vec<f64>,
+    pub(crate) m_1_pow_l: Vec<f64>,
 }
 
 impl std::fmt::Debug for SphericalExpansionByPair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self.parameters)
-    }
-}
-
-
-/// Which gradients are we computing
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(super) struct GradientsOptions {
-    pub positions: bool,
-    pub cell: bool,
-    pub strain: bool,
-}
-
-impl GradientsOptions {
-    pub fn any(self) -> bool {
-        return self.positions || self.cell || self.strain;
     }
 }
 
@@ -186,7 +171,7 @@ impl SphericalExpansionByPair {
     }
 
     /// Compute the product of radial scaling & cutoff smoothing functions
-    fn scaling_functions(&self, r: f64) -> f64 {
+    pub(crate) fn scaling_functions(&self, r: f64) -> f64 {
         let mut scaling = 1.0;
         if let Some(scaler) = self.parameters.density.scaling {
             scaling = scaler.compute(r);
@@ -195,7 +180,7 @@ impl SphericalExpansionByPair {
     }
 
     /// Compute the gradient of the product of radial scaling & cutoff smoothing functions
-    fn scaling_functions_gradient(&self, r: f64) -> f64 {
+    pub(crate) fn scaling_functions_gradient(&self, r: f64) -> f64 {
         let mut scaling = 1.0;
         let mut scaling_grad = 0.0;
         if let Some(scaler) = self.parameters.density.scaling {

python/featomic/featomic/systems/pyscf.py
@@ -91,7 +91,7 @@ class PyscfSystem(SystemBase):
         return self._types
 
     def positions(self):
-        return pyscf.data.nist.BOHR * self._system.atom_coords()
+        return self._system.atom_coords("angstrom")
 
     def cell(self):
         if self.is_periodic: