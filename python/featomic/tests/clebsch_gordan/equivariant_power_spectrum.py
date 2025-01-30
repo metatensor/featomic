5b04644d15d71d4d457579337af841b4146c5ab8
Matthias Kellner <62180584+bananenpampe@users.noreply.github.com>
2025-01-30 13:01:57 +0100

    Adds sample selection to equivariant power spectrum (#376)

--- python/featomic/tests/clebsch_gordan/equivariant_power_spectrum.py
+++ python/featomic/tests/clebsch_gordan/equivariant_power_spectrum.py
@@ -136,6 +136,43 @@ def test_equivariant_power_spectrum_neighbors_to_properties():
     metatensor.equal_raise(powspec_1, powspec_2)
 
 
+def test_sample_selection() -> None:
+    """Tests that the sample selection works as expected.
+    By first computing the powerspectruim for all atoms in H2O
+    Then first for atom 1 and then atom 2  and 3.
+    Their join should be identical to computing it for all atoms.
+    """
+
+    frame = h2o_periodic()
+
+    powspec_calc = EquivariantPowerSpectrum(SphericalExpansion(**SPHEX_HYPERS_SMALL))
+
+    label_1st = metatensor.Labels(
+        ["system", "atom"], np.array([[0, 0]], dtype=np.int32)
+    )
+
+    label_2nd = metatensor.Labels(
+        ["system", "atom"], np.array([[0, 1], [0, 2]], dtype=np.int32)
+    )
+
+    powspec_1 = powspec_calc.compute(
+        frame, neighbors_to_properties=True, selected_samples=label_1st
+    )
+
+    powspec_2 = powspec_calc.compute(
+        frame, neighbors_to_properties=True, selected_samples=label_2nd
+    )
+
+    powspec_3 = metatensor.join(
+        [powspec_1, powspec_2], axis="samples", remove_tensor_name=True
+    )
+    powspec_4 = powspec_calc.compute(frame, neighbors_to_properties=True)
+
+    assert metatensor.equal(powspec_3, powspec_4)
+    assert not metatensor.equal(powspec_2, powspec_4)
+    assert not metatensor.equal(powspec_1, powspec_4)
+
+
 def test_fill_types_option() -> None:
     """
     Test that ``neighbor_types`` options adds arbitrary atomic neighbor types.
