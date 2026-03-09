164a51c69afce7795183c6e32d19bf53ab187791
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-03-09 15:44:35 +0100

    Remove the dependency on pip2pi for build tests

--- scripts/build-all-wheels.sh
+++ scripts/build-all-wheels.sh
@@ -16,30 +16,19 @@ FEATOMIC_VERSION=$(basename "$(find "$TMP_DIR"/dist -name "featomic-*.tar.gz")" 
 FEATOMIC_VERSION=${FEATOMIC_VERSION%.tar.gz}
 
 # for featomic-torch, we need a pre-built version of featomic, so
-# we use the one we just generated and make it available to pip
-dir2pi --no-symlink "$TMP_DIR"/dist
+# we use the one we just generated and make it available to pip via PIP_FIND_LINKS
 
-PORT=8912
-if nc -z localhost $PORT; then
-    printf "\033[91m ERROR: an application is listening to port %d. Please free up the port first. \033[0m\n" $PORT >&2
-    exit 1
-fi
+# Get absolute path to the dist directory
+# We use python here to ensure we get a valid absolute path regardless of platform/shell
+DIST_DIR=$(python -c "import os, sys; print(os.path.abspath(sys.argv[1]))" "$TMP_DIR/dist")
 
-PYPI_SERVER_PID=""
-function cleanup() {
-    kill $PYPI_SERVER_PID
-}
-# Make sure to stop the Python server on script exit/cancellation
-trap cleanup INT TERM EXIT
+# add the dist directory to the set of find links
+# PIP_FIND_LINKS allows pip to find packages in a flat directory
+export PIP_FIND_LINKS="file://$DIST_DIR ${PIP_FIND_LINKS:-}"
 
-python -m http.server --directory "$TMP_DIR"/dist $PORT &
-PYPI_SERVER_PID=$!
-
-# add the python server to the set of extra pip index URL
-export PIP_EXTRA_INDEX_URL="http://localhost:$PORT/simple/ ${PIP_EXTRA_INDEX_URL=}"
 # force featomic-torch to use a specific featomic version when building
 export FEATOMIC_TORCH_BUILD_WITH_FEATOMIC_VERSION="$FEATOMIC_VERSION"
 
-# build featomic-torch, using featomic from `PIP_EXTRA_INDEX_URL`
+# build featomic-torch, using featomic from `PIP_FIND_LINKS`
 # for the sdist => wheel build.
 python -m build python/featomic_torch --outdir "$TMP_DIR/dist"
